5894609d587d1a2e921e80a649eec27b9a86711b
Lucas Wang <lucaschiwang@gmail.com>
2024-03-09 15:23:21 +1100

    Added google map.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { GoogleMapsModule } from '@angular/google-maps';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GoogleMapsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'events';
+
+  zoom = 120;
+  center: google.maps.LatLngLiteral = { lat: 1, lng: 1 };
+  options: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8,
+  };
+
+  ngOnInit() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+      });
+    } else {
+      console.log("navigator.geolocation doesn't exists")
+    }
+  }
+
+  zoomIn() {
+    if (this.zoom < this.options.maxZoom!) this.zoom++;
+  }
+
+  zoomOut() {
+    if (this.zoom > this.options.minZoom!) this.zoom--;
+  }
+
+  currentLocation() {
+    (navigator.geolocation as Geolocation).getCurrentPosition((position: GeolocationPosition) => {
+      this.center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+    });
+  }
+
 }
